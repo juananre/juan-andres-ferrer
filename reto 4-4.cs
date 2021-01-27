5e9cc282ab18dbba2b5baa1a9a219f6618afedbe
juananre <78058130+juananre@users.noreply.github.com>
2021-01-26 20:23:44 -0500

    Add files via upload

---
+++ reto 4-4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace reto_4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // datos 
+
+            Console.Write("ingresa w:");
+            double w = double.Parse(Console.ReadLine());
+            Console.Write("ingrese t:");
+            double t = double.Parse(Console.ReadLine());
+            Console.Write("ingrese angulo c:");
+            double c = double.Parse(Console.ReadLine());
+
+            //calculo de datos 
+
+            double rc = c * Math.PI / 180;
+
+            double y = Math.Cos(rc) * t;
+            double z = Math.Sin(rc) * t;
+
+            double xy = Math.Sqrt(((w * w)-(z * z)));
+
+            double x = xy - y;
+
+            //salida 
+
+            Console.WriteLine("el lado x es: " + x);
+            Console.WriteLine("y es: " + y);
+            Console.WriteLine("z es: " + z);
+            Console.WriteLine("la hipo es: " + xy);
+
+            //https://youtu.be/0L7mRv4XCy8
+        }
+    }
+}
