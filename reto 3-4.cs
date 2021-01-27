4a0bf294578679203d31f76579f80ee1e879958a
juananre <78058130+juananre@users.noreply.github.com>
2021-01-26 20:24:15 -0500

    Add files via upload

---
+++ reto 3-4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace reto_3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //ingresos de datos 
+
+            Console.Write("angulo d:");
+            double Ad = double.Parse(Console.ReadLine());
+            Console.Write("ingrese x:");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("ingrese w:");
+            double w = double.Parse(Console.ReadLine());
+
+            //calculo de datos 
+
+            double rad = Ad * Math.PI / 180;
+
+            double xy = Math.Cos(rad) * w;
+            double y = xy - x;
+
+            //salida de datos 
+
+            Console.WriteLine("el lado y es: " + y);
+
+            //https://youtu.be/koRUymJlbZ0
+        }
+    }
+}
