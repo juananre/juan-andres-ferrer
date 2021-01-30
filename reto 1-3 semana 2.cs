d65eeffbd2e1abfa773a5f513752109cb912cdd0
juananre <78058130+juananre@users.noreply.github.com>
2021-01-30 14:36:41 -0500

    Add files via upload

---
+++ reto 1-3 semana 2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace reto_1_3_semana_4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // entrada 
+            Console.Write("Ingrese su salario: ");
+            int salario = int.Parse(Console.ReadLine());
+            int smmls = 908526;
+
+            if (salario < smmls*2 )
+            {
+                Console.Write("su tarifa es a ");
+            }
+            else if ( smmls*2 <= salario && salario < smmls*4)
+            {
+                Console.Write("su tarifa es b ");
+            }
+            if (salario >= smmls *4)
+            {
+                Console.Write("su tarifa es c ");
+            }
+
+            //https://youtu.be/4ufSuaiGC1E
+        }
+    }
+}
